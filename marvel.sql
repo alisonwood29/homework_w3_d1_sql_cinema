187da88d4373a661b3d02c64c504788e37ce4951
Alison Wood <alisonwood29@gmail.com>
2017-12-04 16:43:26 +0000

    questions 6 & 7

--- marvel.sql
+++ marvel.sql
@@ -146,22 +146,33 @@ INSERT INTO attendances (person_id, movie_id) VALUES (3, 4);
 
 -- 1. Return ALL the data in the 'movies' table.
 SELECT * FROM movies;
+
 -- 2. Return ONLY the name column from the 'people' table
 SELECT name FROM people;
+
 -- 3. Oops! Someone at CodeClan spelled Lorna's name wrong! Change it to reflect the proper spelling (Noble)
 UPDATE people SET name = 'Lorna Noble-Prize'
 WHERE name = 'Lorna Nobel-Prize';
-
 -- SELECT name FROM people;
+
 -- 4. Return ONLY your name from the 'people' table.
 SELECT name FROM people
 WHERE name = 'Alison Wood';
+
 -- 5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 DELETE FROM movies
 WHERE title = 'Batman Begins';
-SELECT * FROM movies;
+-- SELECT * FROM movies;
+
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors
+INSERT INTO people (name)
+VALUES ('John Harper');
+-- SELECT name FROM people;
 
 -- 7. Alex has decided to hijack our movie evening, Remove him from the table of people.
+DELETE FROM people
+WHERE name = 'Alex Bazlinton';
+SELECT * FROM people;
+
 -- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
 -- 9. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 17:55 to 21:30
